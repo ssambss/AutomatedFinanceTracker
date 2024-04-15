c05354f8dcd9741145e0d0a2cfc0a970a29c3a91
ssambss <samppa.kemppainen@hotmail.com>
2024-04-15 20:27:54 +0300

    Including budgets to sheet import, calculating the surplus/deficit (per category) for the month using budgeted values and actual values

--- financeTracker.py
+++ financeTracker.py
@@ -20,15 +20,15 @@ def set_transaction_categories(df, categories: dict) -> None:
                     category_set = True
                     break
         
-        if category_set == False:
+        if not category_set:
             set_categories.append('Muu')
 
 
-def set_savings_to_positive(df) -> None:
+def set_number_to_positive(df) -> None:
     for index, row in df.iterrows():
-        if row['Määrä'] < 0 and row['Kategoria'] == 'Säästö':
+        if row['Määrä'] < 0:
             df.at[index, 'Määrä'] = df.at[index, 'Määrä'] * -1
-
+    
 
 def calculate_total_monthly_sum(df) -> float:
     return df['Määrä'].sum()
@@ -42,7 +42,6 @@ def load_csv_file(csv_file_path: list, selected_columns: list) -> pd.DataFrame:
     return pd.read_csv(csv_file_path, delimiter=';', usecols=selected_columns)
 
 
-# Insert the column names and values into a list of lists to use with gspread update method
 def prepare_data_for_google_sheets(df) -> list:
     columns = [df.columns.values.tolist()]
     values = df.values.tolist()
@@ -61,8 +60,14 @@ def csv_files_to_manipulate() -> list:
     
     return chosen_csv_files_paths
 
-def set_budget_for_category(category: str, budget: float) -> None:
-    
+def define_budgets() -> None:
+    for category in categories.keys():
+        if category == 'Tulo':
+            continue
+        budget = float(input(f'Enter the budget for {category}: '))
+        set_budget_for_category(category, budget)
+
+def set_budget_for_category(category: str, budget: float) -> None:    
     budgets.update({category: budget})
 
 
@@ -79,25 +84,20 @@ categories = {"Säästö": [os.getenv('saving_account')],
             "Tulo": [os.getenv('salary')],
             "Ruoka": ["LIDL", "K-MARKET", "K-CITYMARKET", "PRISMA", "S-MARKET", "K-SUPERMARKET", "ALEPA", "SALE"], 
             "Laskut": ["ELISA", "SATS", "VIHREÄ ÄLYENERGIA", "DNA", "SPOTIFY", "INSINÖÖRILIITTO", "TURVA", "HELEN", "SANOMA"],
-            "Vuokra": [os.getenv('rent')]
+            "Vuokra": [os.getenv('rent')],
+            "Muu": []
             }
-
-budgets = {}
-
-[set_budget_for_category(category, 0) for category in categories.keys()]
-
-print(budgets)
-
-
     
-    #Load the CSV file into a pandas dataframe, set categories for transactions and create dataframes for the total sum per category and monthly total sum 
+budgets = {}
+define_budgets()
+budgets_df = pd.DataFrame(budgets.items(), columns=['Kategoria', 'Budjetti']).sort_values('Kategoria')
+
+print(budgets_df)
 
 
 chosen_csv_files_paths = []
 csv_files = [] 
 chosen_csv_files_paths = csv_files_to_manipulate()
-
-# Define the columns to be imported from the CSV file
 selected_columns = [0, 1, 5]
 
 for i in range(len(chosen_csv_files_paths)):
@@ -108,12 +108,13 @@ for i in range(len(chosen_csv_files_paths)):
     set_transaction_categories(df, categories)
     df['Kategoria'] = set_categories
 
-    #set_savings_to_positive(df)
-
     df_categories_total = calculate_total_sum_per_category(df)
-
+    df_categories_total = df_categories_total[df_categories_total['Kategoria'] != 'Tulo']
+    df_categories_total = pd.merge(df_categories_total, budgets_df, on='Kategoria', how='left')
+    df_categories_total = df_categories_total[['Kategoria', 'Budjetti', 'Määrä']]
+    set_number_to_positive(df_categories_total)
+    df_categories_total['+-'] = df_categories_total['Budjetti'] - df_categories_total['Määrä']
     monthly_total_sum = calculate_total_monthly_sum(df)
-
     monthly_total_df = pd.DataFrame({'Total': [monthly_total_sum]})
 
 
@@ -124,17 +125,14 @@ for i in range(len(chosen_csv_files_paths)):
     
 
     columns = prepare_data_for_google_sheets(df)[0]
-
     values = prepare_data_for_google_sheets(df)[1]
-
     categories_total_columns = prepare_data_for_google_sheets(df_categories_total)[0]
-
     categories_total_values = prepare_data_for_google_sheets(df_categories_total)[1]
-
     monthly_total_column = prepare_data_for_google_sheets(monthly_total_df)[0]
-
     monthly_total_value = prepare_data_for_google_sheets(monthly_total_df)[1]
 
+    budget_total_columns = prepare_data_for_google_sheets(budgets_df)[0]
+    budget_total_values = prepare_data_for_google_sheets(budgets_df)[1]
     
 
         #Import the data to Google Sheets
@@ -142,9 +140,7 @@ for i in range(len(chosen_csv_files_paths)):
     
 
     file_name_stripped = chosen_csv_files_paths[i].split('\\')[-1].split('.')[0]
-
     worksheet_name = re.findall(r'\d+-\d+', file_name_stripped)[0]
-
     spreadsheet = client.open(spreadsheet_name)
 
     if worksheet_name in [sheet.title for sheet in spreadsheet.worksheets()]:
@@ -153,10 +149,10 @@ for i in range(len(chosen_csv_files_paths)):
         worksheet = spreadsheet.add_worksheet(title=worksheet_name, rows='100', cols='20')
 
     worksheet.update(columns + values)
-
     worksheet.update(categories_total_columns + categories_total_values, 'G5')
-
-    worksheet.update(monthly_total_column + monthly_total_value, 'H12')
-
+    worksheet.update(monthly_total_column + monthly_total_value, 'I12')
+    worksheet.update([['Toteutunut']], 'G3')
 
     print(f'Data for {worksheet_name} has been successfully imported to Google Sheets.')
+
+
