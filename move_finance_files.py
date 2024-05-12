da8411673a34c20419dea019310c6b96947086d3
ssambss <samppa.kemppainen@hotmail.com>
2024-05-12 13:54:06 +0300

    Classes FileMover and FileRenamer

--- move_finance_files.py
+++ move_finance_files.py
@@ -1,16 +1,24 @@
 import shutil
 from pathlib import Path
 
-
-"""
-
-Move files from the current working directory to a destination directory.
-
-"""
-source_dir = Path.cwd()
-dest_dir = Path(r'E:\PythonAutomation\FinanceTracker\Files')
-
-for file in source_dir.glob('*.csv'):
-    if file.name.startswith('KÄYTTÖTILI'):
-        shutil.move(file, dest_dir)
-        print(f'{file} moved to {dest_dir}')
+class FileMover:    
+    """
+    
+    Move files from the source directory to a destination directory.
+    
+    """
+    
+    def __init__(self, source_dir: str, dest_dir: str) -> None:
+        self.source_dir = Path(source_dir)
+        self.dest_dir = Path(dest_dir)
+    
+    def move_files(self, file_extension: str, file_prefix: str) -> None:
+        bank_csv = False
+        for file in self.source_dir.glob(f'*{file_extension}'):
+            if file.name.startswith(file_prefix):
+                shutil.move(file, self.dest_dir)
+                print(f'{file} moved to {self.dest_dir}!')
+                bank_csv = True
+        
+        if not bank_csv:
+            print(f'No files with prefix {file_prefix} found.')
--- rename_finance_files.py
+++ rename_finance_files.py
@@ -1,20 +1,25 @@
 from pathlib import Path
-from datetime import datetime as dt
 import re
 
-root_dir = Path(str(Path.cwd()) + r'\Files')
-bank_name = 'Nordea'
+class FileRenamer:
+    """
 
-def rename_file() -> None:
+    Rename transaction csv files in a directory to BANK-YYYY-MM.csv format.
+    
+    """
+    def __init__(self, root_dir: str, bank_name: str) -> None:
+        self.root_dir = Path(root_dir)
+        self.bank_name = bank_name
 
-    for file in root_dir.iterdir():
-        if file.name.startswith('KÄYTTÖTILI'):
-            year_and_month = re.findall(r'\d+-\d+', file.name)
-            new_stem = f'{bank_name}' + '-' +  year_and_month[0]
-            new_name = new_stem + file.suffix
-            new_filepath = file.with_name(new_name)
-            file.rename(new_filepath)
-            print(f'{file.name} renamed to {new_name}')
+    def rename_file(self, file_prefix: str) -> None:
+        for file in self.root_dir.iterdir():
+            if file.name.startswith(file_prefix):
+                year_and_month = re.findall(r'\d+-\d+', file.name)
+                new_stem = f'{self.bank_name}' + '-' +  year_and_month[0]
+                new_name = new_stem + file.suffix
+                new_filepath = file.with_name(new_name)
+                file.rename(new_filepath)
+                print(f'{file.name} renamed to {new_name}!')
+                
+        
 
-
-rename_file()
